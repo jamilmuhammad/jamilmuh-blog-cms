89bc497691e3628359c7d64f486eec4de5c12bc5
Muhammad Jamil <jamielmuhammadd@gmail.com>
2024-05-17 17:34:33 +0700

    initialie login page

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "@/modules/auth/components/LoginForm";
+
+const SignIn: React.FC = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default SignIn;
