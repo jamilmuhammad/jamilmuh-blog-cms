a615979cfa3614834108d52d0ea793cd5546f49b
Muhammad Jamil <jamielmuhammadd@gmail.com>
2024-05-17 17:04:38 +0700

    update input link add copy by navigator, implement react hook form

--- modules/blog/components/Uploaders/InputLink.tsx
+++ modules/blog/components/Uploaders/InputLink.tsx
@@ -1,40 +1,53 @@
 import { toast } from 'react-toastify';
-// import copy from 'copy-to-clipboard';
 
-import type { FC } from 'react';
+import { Suspense, type FC } from 'react';
+import { UseFormRegister } from 'react-hook-form';
+import { FiCopy } from 'react-icons/fi';
+import { FormData } from '../../types/blog';
 
 type InputLinkProps = {
-	value: string;
+	value: string | null;
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	register: UseFormRegister<FormData>;
+	errors: string | undefined;
 };
 
-export const InputLink: FC<InputLinkProps> = ({ value = 'Not Value' }) => {
+export const InputLink: FC<InputLinkProps> = ({ value, handleChange, register, errors = null }) => {
 	return (
 		<div className='relative w-full'>
 			<label className='relative bg-green-300'>
 				<input
+					{...register("image")}
+					placeholder="Enter image url or upload file image"
 					type='text'
 					name='image'
-					disabled
-					value={value}
-					readOnly
+					value={value || ""}
+					onChange={handleChange}
 					className='w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'
-					// w-full h-10 text-sm text-gray-500 truncate pr-24 bg-gray-100 pl-2 border border-gray-300 rounded-lg overflow-hidden cursor-not-allowed
 				/>
 
-				{/* <button
+				<button
 					type='button'
 					title='Press to copy'
 					className='absolute top-1/2 -translate-y-1/2 block right-1 bg-blue-500 hover:bg-blue-600 transition-colors text-gray-50 text-sm w-20 h-8 rounded-lg shadow-xl shadow-blue-500/50'
-					onClick={() => {
-						const isCopy = copy(value || '');
-						if (isCopy) {
-							toast.success('Copy to clipboard', { theme: 'light' });
+					onClick={
+						() => {
+							navigator.clipboard.writeText(value ?? "")
+								.then(() => {
+									toast.success('Copy to clipboard', { theme: 'light' });
+								})
+								.catch(() => {
+									toast.error('Failed to copy', { theme: 'light' });
+								});
 						}
-					}}
+					}
 				>
-					Copy link
-				</button> */}
+					<Suspense>
+						<FiCopy />
+					</Suspense>
+				</button>
 			</label>
+			{errors && <span>{`${errors}`}</span>}
 		</div>
 	);
 };
--- modules/blog/components/Uploaders/index.ts
+++ modules/blog/components/Uploaders/index.ts
@@ -2,4 +2,3 @@ export * from './ButtonFile';
 export * from './Dropzone';
 export * from './InputLink';
 export * from './PreviewImage';
-export * from './ProgressCard';
