349a157f8654c05e002e5df6ce4821cab30b6237
Muhammad Jamil <jamielmuhammadd@gmail.com>
2024-06-20 10:26:13 +0700

    update upload dropzone to api with auth, initialize mdx upload image handler helper

--- lib/upload-file.lib.ts
+++ lib/upload-file.lib.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 type ImageResponse = {
-	public_id: string;
-	secure_url: string;
+	url: string;
 };
+
+type ApiResponse = {
+	data: ImageResponse
+	status: boolean
+}
 
 type UploadFileProps = {
 	formData: FormData | null;
@@ -11,16 +16,53 @@ type UploadFileProps = {
 };
 
 export const uploadFile = async ({ formData, onUploadProgress }: UploadFileProps): Promise<ImageResponse> => {
-	const { data } = await axios.request<ImageResponse>({
+	const { data } = await axios.request<ApiResponse>({
 		method: 'POST',
 		headers: { 'Content-Type': 'multipart/form-data' },
-		url: process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL || '',
+		url: `/api/upload`,
 		data: formData,
 		onUploadProgress(progressEvent) {
 			const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total!);
 			onUploadProgress(percentCompleted);
 		},
+	});	
+
+	return { url: data?.data?.url };
+};
+
+export async function imageUploadHandler(file: File) {
+	const formData = new FormData();
+	formData.append('file', file);
+
+	const promise = fetch(`/api/upload`, {
+		method: 'POST',
+		body: formData,
 	});
 
-	return { secure_url: data.secure_url, public_id: data.public_id };
-};
+	toast.promise(
+		promise,
+		{
+			pending: 'Uploading image...',
+			success: 'Image uploaded successfully.',
+			error: 'Error uploading image',
+		}
+	);
+
+	try {
+		const res = await promise;
+
+		// Successfully uploaded image
+		if (res.status === 200) {
+			const data = await res.json();
+			
+			return data.data.url;
+		} else if (res.status === 401) {
+			throw new Error(res.statusText);
+		} else {
+			throw new Error('Error uploading image. Please try again.');
+		}
+	} catch (error) {
+		console.error('Error uploading image:', error);
+		throw error;
+	}
+}
