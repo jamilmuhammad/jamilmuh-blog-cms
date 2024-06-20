d375052ab84946f9f14c255c5d294806d2fd4aae
Muhammad Jamil <jamielmuhammadd@gmail.com>
2024-06-20 09:37:04 +0700

    update migrate clientside layout to a component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
-"use client";
 import "./globals.css";
 import "./data-tables-css.css";
 import "./satoshi.css";
-import { useState, useEffect } from "react";
-import MainLayout from "@/components/Layouts/MainLayout";
 import ToastProvider from "@/components/Providers/Toast";
-import GuestLayout from "@/components/Layouts/GuestLayout";
-import Cookies from 'js-cookie';
-import { useRouter } from "next/navigation";
+import Layout from "@/components/Layouts/Layout";
 
 export default function RootLayout({
   children,
@@ -15,35 +10,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
 
-  const access_token = Cookies.get('access_token')
-
-  const [validToken, setValidToken] = useState(false);
-
-  const router = useRouter()
-
-  useEffect(() => {
-    if (access_token) {
-      setValidToken(true);
-    }
-  }, [access_token]);
-
-  useEffect(() => {
-    if (!validToken) {
-      router.refresh();
-    }
-  }, [validToken]);
-
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
         <ToastProvider>
-          <div className="dark:bg-boxdark-2 dark:text-bodydark">
-            {validToken ? (
-              <MainLayout>{children}</MainLayout>
-            ) : (
-              <GuestLayout>{children}</GuestLayout>
-            )}
-          </div>
+          <Layout>
+            {children}
+          </Layout>
         </ToastProvider>
       </body>
     </html>
